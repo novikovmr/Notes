2db23b28c96712f687f152cf981142dd49b82c33
novikovmr <maksim101101@gmail.com>
2024-02-23 10:28:09 +0400

    Обновил функционал

--- Controller/commands.py
+++ Controller/commands.py
@@ -1,8 +1,71 @@
 import Notes
+import Logs.load as load
+import Logs.write as write
 
 
 def add_note():
     title = input("Введите заголовок:\n")
     content = input ("Введите содержимое заметки: \n")
     note = Notes.Note(title=title, content=content)
-    
+    notes_arr = load.read_file()
+    for i in notes_arr:
+        if Notes.Note.get_id(note) == Notes.Note.get_id(i):
+            Notes.Note.set_id(note)
+
+    notes_arr.append(note)
+    write.write_file(notes_arr, 'a')
+    print('Заметка добавлена')
+
+
+def print_note(txt):
+    notes_arr = load.read_file()
+
+    if notes_arr:
+        if txt == "all":
+            print("Журнал заметок:")
+            for i in notes_arr:
+                print(Notes.Note.map_note(i))
+
+        elif txt == "ID":
+            for i in notes_arr:
+                print("ID: ", Notes.Note.get_id(i))
+            id = input("\nВведите id заметки: ")
+            flag = True
+            for i in notes_arr:
+                if id == Notes.Note.get_id(i):
+                    print(Notes.Note.map_note(i))
+                    flag = False
+            if flag:
+                print("Нет такого id")
+
+        elif txt == "date":
+            date = input("Введите дату в формате: dd.mm.yyyy: ")
+            flag = True
+            for i in notes_arr:
+                date_note = str(Notes.Note.get_date(i))
+                if date == date_note[:10]:
+                    print(Notes.Note.map_note(i))
+                    flag = False
+            if flag:
+                print("Нет такой даты")
+        else:
+            print("Журнал заметок пустой!")
+
+def change_note():
+    id = input("Введите ID изменяемой заметки: ")
+    notes_arr = load.read_file()
+    flag = True
+    notes_arr_new = []
+    for i in notes_arr:
+        if id == Notes.Note.get_id(i):
+            i.title = input("измените  заголовок:\n")
+            i.body = input("измените  описание:\n")
+            Notes.Note.set_date(i)
+            logic = False
+        notes_arr_new .append(i)
+
+    if flag:
+        write.write_file(notes_arr_new , 'a')
+        print("Заметка с id: ", id, " успешно изменена!")
+    else:
+        print("нет такого id")
--- Notes.py
+++ Notes.py
@@ -11,32 +11,32 @@ class Note:
         self.content = content
         self.date = date
 
-        def get_id(note):
+    def get_id(note):
             return note.id
 
-        def get_title(note):
+    def get_title(note):
             return note.title
 
-        def get_content(note):
+    def get_content(note):
             return note.content
 
-        def get_date(note):
+    def get_date(note):
             return note.date
 
-        def set_id(note):
+    def set_id(note):
             note.id = str(idCounter.counter())
 
-        def set_title(note):
+    def set_title(note):
             note.title = note
 
-        def set_content(note):
+    def set_content(note):
             note.content = note
 
-        def set_date(note):
+    def set_date(note):
             note.date = str(datetime.now().strftime("%d.%m.%Y %H:%M:%S"))
 
-        def to_string(note):
+    def to_string(note):
             return note.id + ';' + note.title + ';' + note.content + ';' + note.date
 
-        def map_note(note):
+    def map_note(note):
             return '\nID: ' + note.id + '\n' + 'Заголовок: ' + note.title + '\n' + 'Описание: ' + note.content + '\n' + 'Дата публикации: ' + note.date
