dfb57da295c07bbe6c9329d21dac0557c799a5c0
novikovmr <maksim101101@gmail.com>
2024-02-23 10:01:42 +0400

    Начало работы

---
+++ Notes.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+
+import idCounter
+
+
+class Note:
+    def __init__(self, id = str(idCounter.counter()), title='текст', content='текст',
+                 date=str(datetime.now().strftime("%d.%m.%Y %H:%M:%S"))):
+        self.id = id
+        self.title = title
+        self.content = content
+        self.date = date
+
+        def get_id(note):
+            return note.id
+
+        def get_title(note):
+            return note.title
+
+        def get_content(note):
+            return note.content
+
+        def get_date(note):
+            return note.date
+
+        def set_id(note):
+            note.id = str(idCounter.counter())
+
+        def set_title(note):
+            note.title = note
+
+        def set_content(note):
+            note.content = note
+
+        def set_date(note):
+            note.date = str(datetime.now().strftime("%d.%m.%Y %H:%M:%S"))
